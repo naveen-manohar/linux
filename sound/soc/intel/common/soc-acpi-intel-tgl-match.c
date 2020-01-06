4e39e2b70ea39a45ed6969d7135bc428eebf3541
Naveen Manohar <naveen.m@intel.com>
2020-01-06 15:05:44 +0530

    [DEBUG]: TGL RT5682 SDW mach drv load
    Change-Id: I0904c0e17afdcfe2eabbe04a3e4c0eda5c7b11aa
    Signed-off-by: Naveen Manohar <naveen.m@intel.com>

--- sound/soc/intel/common/soc-acpi-intel-tgl-match.c
+++ sound/soc/intel/common/soc-acpi-intel-tgl-match.c
@@ -9,20 +9,20 @@
 #include <sound/soc-acpi.h>
 #include <sound/soc-acpi-intel-match.h>
 
-static struct snd_soc_acpi_codecs tgl_codecs = {
+/*static struct snd_soc_acpi_codecs tgl_codecs = {
 	.num_codecs = 1,
 	.codecs = {"MX98357A"}
 };
-
+*/
 struct snd_soc_acpi_mach snd_soc_acpi_intel_tgl_machines[] = {
-	{
-		.id = "10EC1308",
-		.drv_name = "rt711_rt1308",
-		.link_mask = 0x1, /* RT711 on SoundWire link0 */
-		.sof_fw_filename = "sof-tgl.ri",
-		.sof_tplg_filename = "sof-tgl-rt711-rt1308.tplg",
-	},
-	{
+//	{
+//		.id = "10EC1308",
+//		.drv_name = "rt711_rt1308",
+//		.link_mask = 0x1, /* RT711 on SoundWire link0 */
+//		.sof_fw_filename = "sof-tgl.ri",
+//		.sof_tplg_filename = "sof-tgl-rt711-rt1308.tplg",
+//	},
+/*	{
 		.id = "10EC5682",
 		.drv_name = "tgl_max98357a_rt5682",
 		.machine_quirk = snd_soc_acpi_codec_list,
@@ -30,17 +30,17 @@ struct snd_soc_acpi_mach snd_soc_acpi_intel_tgl_machines[] = {
 		.sof_fw_filename = "sof-tgl.ri",
 		.sof_tplg_filename = "sof-tgl-max98357a-rt5682.tplg",
 	},
-	{},
+*/	{},
 };
 EXPORT_SYMBOL_GPL(snd_soc_acpi_intel_tgl_machines);
 
 struct snd_soc_acpi_mach snd_soc_acpi_intel_tgl_sdw_machines[] = {
-	{
-		.link_mask = 0x1, /* this will only enable rt711 for now */
-		.drv_name = "sdw_rt1308",
-		.sof_fw_filename = "sof-tgl.ri",
-		.sof_tplg_filename = "sof-tgl-rt1308.tplg",
-	},
+//	{
+//		.link_mask = 0x1, /* this will only enable rt711 for now */
+//		.drv_name = "sdw_rt711_rt1308_rt715",
+//		.sof_fw_filename = "sof-tgl.ri",
+//		.sof_tplg_filename = "sof-tgl-rt711-rt1308-rt715.tplg",
+//	},
 	{
 		.link_mask = 0x1, /* this will only enable rt5682 for now */
 		.drv_name = "tgl_sdw_rt5682",
