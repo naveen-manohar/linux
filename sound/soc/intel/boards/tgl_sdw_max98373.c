af4bdc7e708d1a3c343bbeb43ef137ea6971d7fb
Naveen Manohar <naveen.m@intel.com>
2020-01-16 16:14:00 +0530

    Status-Quo Definition
    Change-Id: Id0156c8d396eb0656a5b30a9cc9597892bb16107
    Signed-off-by: Naveen Manohar <naveen.m@intel.com>

--- sound/soc/intel/boards/tgl_sdw_max98373.c
+++ sound/soc/intel/boards/tgl_sdw_max98373.c
@@ -26,6 +26,10 @@
 #include <sound/soc-acpi.h>
 #include "../../codecs/hdac_hdmi.h"
 
+#define MAX98373_CODEC_DAI      "max98373-aif1"
+#define MAXIM_DEV0_NAME         "sdw:1:19f:8373:0:3"
+#define MAXIM_DEV1_NAME         "sdw:1:19f:8373:0:7"
+
 struct mc_private {
 	struct list_head hdmi_pcm_list;
 };
@@ -47,14 +51,14 @@ static int card_late_probe(struct snd_soc_card *card)
 #endif
 
 static const struct snd_soc_dapm_widget widgets[] = {
-	SND_SOC_DAPM_SPK("Speaker", NULL),
-//	SND_SOC_DAPM_SPK("Right Spk", NULL),
+	SND_SOC_DAPM_SPK("Left Spk", NULL),
+	SND_SOC_DAPM_SPK("Right Spk", NULL),
 };
 
 static const struct snd_soc_dapm_route map[] = {
 	/* Speakers */
-	{ "Speaker", NULL, "Left BE_OUT" },
-	{ "Speaker", NULL, "Right BE_OUT" },
+	{ "Left Spk ", NULL, "Left BE_OUT" },
+	{ "Right Spk", NULL, "Right BE_OUT" },
 };
 
 //static const struct snd_soc_dapm_route second_speaker_map[] = {
@@ -63,8 +67,8 @@ static const struct snd_soc_dapm_route map[] = {
 //};
 
 static const struct snd_kcontrol_new controls[] = {
-	SOC_DAPM_PIN_SWITCH("Speaker"),
-//	SOC_DAPM_PIN_SWITCH("Right Spk"),
+	SOC_DAPM_PIN_SWITCH("Left Spk"),
+	SOC_DAPM_PIN_SWITCH("Right Spk"),
 };
 
 /*
@@ -92,22 +96,51 @@ SND_SOC_DAILINK_DEF(sdw1_pin2,
 	DAILINK_COMP_ARRAY(COMP_CPU("SDW1 Pin2")));
 SND_SOC_DAILINK_DEF(sdw1_codec,
 //	DAILINK_COMP_ARRAY(COMP_CODEC("sdw:1:19f:8373:0:3", "max98373-aif1")));
-	DAILINK_COMP_ARRAY(COMP_CODEC("sdw:1:19f:8373:0:3", "max98373-aif1"),
-			COMP_CODEC("sdw:1:19f:8373:0:7", "max98373-aif1")));
+	DAILINK_COMP_ARRAY(COMP_CODEC(MAXIM_DEV0_NAME, MAX98373_CODEC_DAI),
+			COMP_CODEC(MAXIM_DEV1_NAME, MAX98373_CODEC_DAI)));
 
 SND_SOC_DAILINK_DEF(platform,
 		DAILINK_COMP_ARRAY(COMP_PLATFORM("0000:00:1f.3")));
 
 static struct snd_soc_codec_conf codec_conf[] = {
 	{
-		.dlc = COMP_CODEC_CONF("sdw:1:19f:8373:0:3"),
+		.dlc = COMP_CODEC_CONF(MAXIM_DEV0_NAME),
 		.name_prefix = "Right",
 	},
 	{
-		.dlc = COMP_CODEC_CONF("sdw:1:19f:8373:0:7"),
+		.dlc = COMP_CODEC_CONF(MAXIM_DEV1_NAME),
 		.name_prefix = "Left",
 	},
 
+};
+
+static int max98373_hw_params(struct snd_pcm_substream *substream,
+	struct snd_pcm_hw_params *params)
+{
+	struct snd_soc_pcm_runtime *rtd = substream->private_data;
+	struct snd_soc_dai *codec_dai;
+	int srate, i, ret = 0;
+
+	srate = params_rate(params);
+
+	for (i = 0; i < rtd->num_codecs; i++) {
+		codec_dai = rtd->codec_dais[i];
+
+		/*
+		 *  Codec TDM is configured as 2 CH over 2 codecs
+		 */
+		if (!strcmp(codec_dai->component->name, MAXIM_DEV0_NAME))
+			codec_dai->tx_mask = 0x1;
+		if (!strcmp(codec_dai->component->name, MAXIM_DEV1_NAME))
+			codec_dai->tx_mask = 0x2;
+	}
+
+	return ret;
+
+}
+
+static const struct snd_soc_ops max98373_ops = {
+	.hw_params = max98373_hw_params,
 };
 
 struct snd_soc_dai_link dailink[] = {
@@ -117,6 +150,7 @@ struct snd_soc_dai_link dailink[] = {
 		.no_pcm = 1,
 		.dpcm_playback = 1,
 		.nonatomic = true,
+		.ops = &max98373_ops,
 		SND_SOC_DAILINK_REG(sdw1_pin2, sdw1_codec, platform),
 	}
 };
@@ -164,8 +198,8 @@ static int mc_probe(struct platform_device *pdev)
 	}
 	printk("naveen %s %d\n", __func__, __LINE__);
 	snd_soc_card_set_drvdata(card, ctx);
-	card->num_links = ARRAY_SIZE(dailink) - 1 ;
-	card->num_configs = ARRAY_SIZE(codec_conf) - 1;
+//	card->num_links = ARRAY_SIZE(dailink) - 1 ;
+//	card->num_configs = ARRAY_SIZE(codec_conf) - 1;
 	printk("naveen %s %d\n", __func__, __LINE__);
 	/* Register the card */
 	ret = devm_snd_soc_register_card(&pdev->dev, card);
