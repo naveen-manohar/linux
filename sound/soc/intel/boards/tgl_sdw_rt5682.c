3c590188a18a1cd08c1a4482e8fae3f535f1ab99
Naveen Manohar <naveen.m@intel.com>
2020-02-26 19:50:11 +0530

    ASoC: Intel: boards: tgl_sdw_rt5682: Add HDMI & DMIC Support
    Add HDMI Audio & DMIC Support
    
    Change-Id: I1a18b5418340c2418f88ebff1afa210dd54c09a6
    Signed-off-by: Naveen Manohar <naveen.m@intel.com>

--- sound/soc/intel/boards/tgl_sdw_rt5682.c
+++ sound/soc/intel/boards/tgl_sdw_rt5682.c
@@ -25,16 +25,19 @@
 #include <sound/soc.h>
 #include <sound/soc-acpi.h>
 #include "../../codecs/hdac_hdmi.h"
+#include "hda_dsp_common.h"
 
 struct snd_soc_jack rt5682_headset;
 
-struct mc_private {
+struct tgl_card_private {
 	struct list_head hdmi_pcm_list;
+	bool common_hdmi_codec_drv;
 };
 
-#if IS_ENABLED(CONFIG_SND_SOC_HDAC_HDMI)
+#if IS_ENABLED(CONFIG_SND_HDA_CODEC_HDMI)
+static struct snd_soc_jack tgl_hdmi[4];
 
-struct hdmi_pcm {
+struct tgl_hdmi_pcm {
 	struct list_head head;
 	struct snd_soc_dai *codec_dai;
 	int device;
@@ -43,9 +46,71 @@ struct hdmi_pcm {
 #define NAME_SIZE	32
 static int card_late_probe(struct snd_soc_card *card)
 {
+	struct tgl_card_private *ctx = snd_soc_card_get_drvdata(card);
+	struct tgl_hdmi_pcm *pcm;
+	struct snd_soc_component *component = NULL;
+	int err, i = 0;
+	char jack_name[NAME_SIZE];
+
+	if (list_empty(&ctx->hdmi_pcm_list))
+		return -EINVAL;
+
+	if (ctx->common_hdmi_codec_drv) {
+		pcm = list_first_entry(&ctx->hdmi_pcm_list, struct tgl_hdmi_pcm,
+				       head);
+		component = pcm->codec_dai->component;
+		return hda_dsp_hdmi_build_controls(card, component);
+	}
+
+	list_for_each_entry(pcm, &ctx->hdmi_pcm_list, head) {
+		component = pcm->codec_dai->component;
+		snprintf(jack_name, sizeof(jack_name),
+			 "HDMI/DP, pcm=%d Jack", pcm->device);
+		err = snd_soc_card_jack_new(card, jack_name,
+					    SND_JACK_AVOUT, &tgl_hdmi[i],
+					    NULL, 0);
+
+		if (err)
+			return err;
+
+		err = hdac_hdmi_jack_init(pcm->codec_dai, pcm->device,
+					  &tgl_hdmi[i]);
+		if (err < 0)
+			return err;
+
+		i++;
+	}
+
+	if (!component)
+		return -EINVAL;
+
+	return hdac_hdmi_jack_port_init(component, &card->dapm);
+}
+#else
+static int card_late_probe(struct snd_soc_card *card)
+{
 	return 0;
 }
 #endif
+
+static int tgl_hdmi_init(struct snd_soc_pcm_runtime *rtd)
+{
+	struct tgl_card_private *ctx = snd_soc_card_get_drvdata(rtd->card);
+	struct snd_soc_dai *dai = rtd->codec_dai;
+	struct tgl_hdmi_pcm *pcm;
+
+	pcm = devm_kzalloc(rtd->card->dev, sizeof(*pcm), GFP_KERNEL);
+	if (!pcm)
+		return -ENOMEM;
+
+	/* dai_link id is 1:1 mapped to the PCM device */
+	pcm->device = rtd->dai_link->id;
+	pcm->codec_dai = dai;
+
+	list_add_tail(&pcm->head, &ctx->hdmi_pcm_list);
+
+	return 0;
+}
 
 static struct snd_soc_jack_pin sdw_jack_pins[] = {
 	{
@@ -128,10 +193,35 @@ SND_SOC_DAILINK_DEF(sdw0_codec,
 SND_SOC_DAILINK_DEF(platform,
 		DAILINK_COMP_ARRAY(COMP_PLATFORM("0000:00:1f.3")));
 
+SND_SOC_DAILINK_DEF(dmic_pin,
+	DAILINK_COMP_ARRAY(COMP_CPU("DMIC01 Pin")));
+SND_SOC_DAILINK_DEF(dmic_codec,
+	DAILINK_COMP_ARRAY(COMP_CODEC("dmic-codec", "dmic-hifi")));
+
+SND_SOC_DAILINK_DEF(idisp1_pin,
+	DAILINK_COMP_ARRAY(COMP_CPU("iDisp1 Pin")));
+SND_SOC_DAILINK_DEF(idisp1_codec,
+	DAILINK_COMP_ARRAY(COMP_CODEC("ehdaudio0D2", "intel-hdmi-hifi1")));
+
+SND_SOC_DAILINK_DEF(idisp2_pin,
+	DAILINK_COMP_ARRAY(COMP_CPU("iDisp2 Pin")));
+SND_SOC_DAILINK_DEF(idisp2_codec,
+	DAILINK_COMP_ARRAY(COMP_CODEC("ehdaudio0D2", "intel-hdmi-hifi2")));
+
+SND_SOC_DAILINK_DEF(idisp3_pin,
+	DAILINK_COMP_ARRAY(COMP_CPU("iDisp3 Pin")));
+SND_SOC_DAILINK_DEF(idisp3_codec,
+	DAILINK_COMP_ARRAY(COMP_CODEC("ehdaudio0D2", "intel-hdmi-hifi3")));
+
+SND_SOC_DAILINK_DEF(idisp4_pin,
+	DAILINK_COMP_ARRAY(COMP_CPU("iDisp4 Pin")));
+SND_SOC_DAILINK_DEF(idisp4_codec,
+	DAILINK_COMP_ARRAY(COMP_CODEC("ehdaudio0D2", "intel-hdmi-hifi4")));
+
 struct snd_soc_dai_link dailink[] = {
 	{
 		.name = "SDW0-Playback",
-		.id = 2,
+		.id = 0,
 		.init = rt5682_codec_init,
 		.no_pcm = 1,
 		.dpcm_playback = 1,
@@ -140,12 +230,54 @@ struct snd_soc_dai_link dailink[] = {
 	},
 	{
 		.name = "SDW0-Capture",
-		.id = 3,
+		.id = 1,
 		.no_pcm = 1,
 		.dpcm_capture = 1,
 		.nonatomic = true,
 		SND_SOC_DAILINK_REG(sdw0_pin3, sdw0_codec, platform),
 	},
+	{
+		.name = "dmic01",
+		.id = 4,
+		.ignore_suspend = 1,
+		.dpcm_capture = 1,
+		.no_pcm = 1,
+		SND_SOC_DAILINK_REG(dmic_pin, dmic_codec, platform),
+	},
+#if IS_ENABLED(CONFIG_SND_SOC_HDAC_HDMI)
+	{
+		.name = "iDisp1",
+		.id = 5,
+		.init = tgl_hdmi_init,
+		.dpcm_playback = 1,
+		.no_pcm = 1,
+		SND_SOC_DAILINK_REG(idisp1_pin, idisp1_codec, platform),
+	},
+	{
+		.name = "iDisp2",
+		.id = 6,
+		.init = tgl_hdmi_init,
+		.dpcm_playback = 1,
+		.no_pcm = 1,
+		SND_SOC_DAILINK_REG(idisp2_pin, idisp2_codec, platform),
+	},
+	{
+		.name = "iDisp3",
+		.id = 7,
+		.init = tgl_hdmi_init,
+		.dpcm_playback = 1,
+		.no_pcm = 1,
+		SND_SOC_DAILINK_REG(idisp3_pin, idisp3_codec, platform),
+	},
+	{
+		.name = "iDisp4",
+		.id = 8,
+		.init = tgl_hdmi_init,
+		.dpcm_playback = 1,
+		.no_pcm = 1,
+		SND_SOC_DAILINK_REG(idisp4_pin, idisp4_codec, platform),
+	},
+#endif
 };
 
 /* SoC card */
@@ -164,17 +296,21 @@ static struct snd_soc_card card_rt5682 = {
 
 static int mc_probe(struct platform_device *pdev)
 {
-	struct mc_private *ctx;
+	struct tgl_card_private *ctx;
 	struct snd_soc_acpi_mach *mach;
 	const char *platform_name;
 	struct snd_soc_card *card = &card_rt5682;
-	int ret;
+	int ret = 0;
 
 	dev_dbg(&pdev->dev, "Entry %s\n", __func__);
 
 	ctx = devm_kzalloc(&pdev->dev, sizeof(*ctx), GFP_KERNEL);
 	if (!ctx)
 		return -ENOMEM;
+
+#if IS_ENABLED(CONFIG_SND_HDA_CODEC_HDMI)
+	INIT_LIST_HEAD(&ctx->hdmi_pcm_list);
+#endif
 
 	card->dev = &pdev->dev;
 
@@ -187,6 +323,7 @@ static int mc_probe(struct platform_device *pdev)
 		return ret;
 
 	snd_soc_card_set_drvdata(card, ctx);
+	ctx->common_hdmi_codec_drv = mach->mach_params.common_hdmi_codec_drv;
 
 	/* Register the card */
 	ret = devm_snd_soc_register_card(&pdev->dev, card);
