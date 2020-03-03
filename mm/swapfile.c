b788b416980b5a6c15f778a9d42f65a3c0d4988a
Ilkka Koskinen <ilkka.koskinen@intel.com>
2020-03-03 15:13:09 -0800

    CHROMIUM: Allow block devices being used as swap if disk_based_swap is set
    Patch
    "880b2d774046 CHROMIUM: Restrict swapon() to "zram" devices / lock down zram"
    prevents block devices being used as swap even if disk_based_swap has been
    enabled. The flag should be checked also in case of block devices.
    
    BUG=None
    TEST=Tested with Intel Optane H10 using Optane memory part as swap
    
    Change-Id: If69b45752ec1079c7880f199909a21815890ba28
    Signed-off-by: Ilkka Koskinen <ilkka.koskinen@intel.com>

--- mm/swapfile.c
+++ mm/swapfile.c
@@ -2894,7 +2894,7 @@ static int claim_swapfile(struct swap_info_struct *p, struct inode *inode)
 		char name[BDEVNAME_SIZE];
 		p->bdev = bdgrab(I_BDEV(inode));
 		bdevname(p->bdev, name);
-		if (strncmp(name, "zram", strlen("zram"))) {
+		if (!sysctl_disk_based_swap && strncmp(name, "zram", strlen("zram"))) {
 			bdput(p->bdev);
 			p->bdev = NULL;
 			return -EINVAL;
